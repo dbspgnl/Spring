b4b7dd3c80a8e98796b8bafa721e5d6fc461b828
dbspgnl <dbspgnl@naver.com>
2020-03-18 21:50:05 +0900

    Add package02-03

---
+++ Spring04/src/com/test03/MyFood.java
@@ -0,0 +1,34 @@
+package com.test03;
+
+public class MyFood {
+
+	private Food favoriteFood;
+	private Food unFavoriteFood;
+	
+	public MyFood() {
+		this.favoriteFood = new Food("회", 15000);
+		this.unFavoriteFood = new Food("해물탕", 25000);
+	}
+	public MyFood(Food favoriteFood, Food unFavoriteFood) {
+		super();
+		this.favoriteFood = favoriteFood;
+		this.unFavoriteFood = unFavoriteFood;
+	}
+	public Food getFavoriteFood() {
+		return favoriteFood;
+	}
+	public void setFavoriteFood(Food favoriteFood) {
+		this.favoriteFood = favoriteFood;
+	}
+	public Food getUnFavoriteFood() {
+		return unFavoriteFood;
+	}
+	public void setUnFavoriteFood(Food unFavoriteFood) {
+		this.unFavoriteFood = unFavoriteFood;
+	}
+	@Override
+	public String toString() {
+		return "favoriteFood: " + favoriteFood + ", unFavoriteFood: " + unFavoriteFood;
+	}
+	
+}
