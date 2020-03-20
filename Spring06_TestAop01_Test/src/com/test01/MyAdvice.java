d5442193635ff35f0c9e36862d4257711faa99fe
dbspgnl <dbspgnl@naver.com>
2020-03-20 14:42:40 +0900

    fix MyAdvice in Spring06_test

--- Spring06_TestAop01_Test/src/com/test01/MyAdvice.java
+++ Spring06_TestAop01_Test/src/com/test01/MyAdvice.java
@@ -13,13 +13,13 @@ public class MyAdvice {
 	public void beforeSaying(JoinPoint join) {
 		System.out.println("당신의 이름은 무엇입니까?");
 	}
-	@After("execution(public * sayName(..))")
+	@After("execution(public String sayJob(..))")
 	public void afterSaying(JoinPoint join) {
-		System.out.println("직업이 무엇입니까?");
-	}
-	@AfterReturning(pointcut = "execution(public * sayName(..))", returning = "returnVal")
-	public void afterReturnSaying(Object returnVal) {
 		System.out.println("이름이 멋지시네요.");
+	}
+	@AfterReturning(pointcut = "execution(public String sayJob(..))", returning = "returnVal")
+	public void afterReturnSaying(Object returnVal) {
+		System.out.println("직업이 무엇입니까?.");
 	}
 
 }
