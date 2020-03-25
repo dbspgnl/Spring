b1d0490b150c40ac278556ff46d2ceec2b2f9c78
dbspgnl <dbspgnl@naver.com>
2020-03-25 19:32:16 +0900

    New Prj : SpringMVC03++

---
+++ SpringMVC03_filter_aop_login/src/main/java/com/mvc/upgrade/model/dao/MemberDaoImpl.java
@@ -0,0 +1,29 @@
+package com.mvc.upgrade.model.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.mvc.upgrade.model.dto.MemberDto;
+
+@Repository
+public class MemberDaoImpl implements MemberDao {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Override
+	public MemberDto login(MemberDto dto) {
+		MemberDto res = null;
+		System.out.println(">"+dto.getMemberid());
+		System.out.println(">"+dto.getMemberpw());
+		try {
+			res = sqlSession.selectOne(NAMESPACE+"login", dto);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("[ERROR]: MemberDaoImpl selectOne");
+		}
+		return res;
+	}
+
+}
---
+++ SpringMVC03_filter_aop_login_interceptor/src/main/java/com/mvc/upgrade/controller/MemberController.java
@@ -0,0 +1,52 @@
+package com.mvc.upgrade.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mvc.upgrade.model.biz.MemberBiz;
+import com.mvc.upgrade.model.dto.MemberDto;
+
+@Controller
+public class MemberController {
+	private Logger logger = LoggerFactory.getLogger(MemberController.class);
+	@Autowired
+	private MemberBiz biz;
+	@RequestMapping("/loginform.do")
+	public String loginForm() {
+		logger.info("LOGIN FORM");
+		return "mvclogin";
+	}
+	@RequestMapping(value="/ajaxlogin.do", method=RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Boolean> ajaxLogin(HttpSession session,
+			@RequestBody MemberDto dto){
+		logger.info("AJAX LOGIN");
+		/*
+		 * @Response : java 객체를 response 객체에 binding
+		 * (뷰리졸버를 거치지 않고 바로 ajax로 보내준다)
+		 * (서버에서 넘어가는 객체를 jackson이 json 형태로 해준다)
+		 * @RequestBody : request 객체에 담겨져 넘어오는 데이터를 java 객체에 binding
+		 * (json 형태를 알아서 자바로 변경해준다)
+		 */
+		MemberDto res = biz.login(dto);
+		boolean check = false;
+		if(res!=null) {
+			session.setAttribute("login", res);
+			check = true;
+		}
+		Map<String, Boolean> map = new HashMap<String, Boolean>();
+		map.put("check", check);
+		return map;
+	}
+}
