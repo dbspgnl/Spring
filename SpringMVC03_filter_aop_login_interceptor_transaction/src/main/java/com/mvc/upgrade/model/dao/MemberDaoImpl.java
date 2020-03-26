d7f1fc645afc92e1adeb7bb97b70ce5a39217845
dbspgnl <dbspgnl@naver.com>
2020-03-26 21:44:01 +0900

    New Prj : SpringMVC03_transaction

---
+++ SpringMVC03_filter_aop_login_interceptor_transaction/src/main/java/com/mvc/upgrade/model/dao/MemberDaoImpl.java
@@ -0,0 +1,40 @@
+package com.mvc.upgrade.model.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.mvc.upgrade.model.dto.MemberDto;
+
+@Repository
+public class MemberDaoImpl implements MemberDao {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Override
+	public MemberDto login(MemberDto dto) {
+		MemberDto res = null;
+		//System.out.println(">"+dto.getMemberid());
+		//System.out.println(">"+dto.getMemberpw());
+		try {
+			res = sqlSession.selectOne(NAMESPACE+"login", dto);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("[ERROR]: MemberDaoImpl selectOne");
+		}
+		return res;
+	}
+	@Override
+	public int join(MemberDto dto) {
+		int res = 0;
+		try {
+			res = sqlSession.insert(NAMESPACE+"join", dto);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("[ERROR]: MemberDaoImpl insert");
+		}
+		return res;
+	}
+	
+}
